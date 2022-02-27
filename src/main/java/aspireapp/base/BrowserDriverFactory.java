33b491d40978a5f2b51151edea7554a6e885de6a
Ngoc Pham <baongocpt@gmail.com>
2022-02-27 21:56:33 +0700

    add support on mac os

--- src/main/java/aspireapp/base/BrowserDriverFactory.java
+++ src/main/java/aspireapp/base/BrowserDriverFactory.java
@@ -10,30 +10,35 @@ public class BrowserDriverFactory {
 	private ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
 	private String browser;
 	private Logger log;
+	private String resources_path;
+	private String chromedriver_name;
+	
 
 	public BrowserDriverFactory(String browser, Logger log) {
 		this.browser = browser.toLowerCase();
 		this.log = log;
+		this.resources_path = "src/main/resources/";
+		this.chromedriver_name = "chromedriver";
 	}
 
 	public WebDriver createDriver() {
 		// Create driver
 		log.info("Create driver: " + browser);
+		
+		String os = System.getProperty("os.name");
+		if (os.toLowerCase().contains("windows")) {
+			this.chromedriver_name = this.chromedriver_name + ".exe";
+		}
 
 		switch (browser) {
 		case "chrome":
-			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", resources_path + chromedriver_name);
 			driver.set(new ChromeDriver());
-			break;
-
-		case "firefox":
-			System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
-			driver.set(new FirefoxDriver());
 			break;
 
 		default:
 			System.out.println("Do not know how to start: " + browser + ", starting chrome.");
-			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", resources_path + chromedriver_name);
 			driver.set(new ChromeDriver());
 			break;
 		}
